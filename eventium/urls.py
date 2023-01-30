c9b8dda5b9cb7b560be09f0d6d630962016aa19a
Korey Peters <72994607+koreypeters-dexcom@users.noreply.github.com>
2023-01-30 09:24:28 -0700

    Added a root Eventium page

--- eventium/urls.py
+++ eventium/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="eventium-home"),
     path("events/", views.events_list, name="eventium-events-list"),
     path("events/<event_id>", views.events_detail, name="eventium-events-detail"),
     path(
